9b2a9b6dc65bf47c57e6241353ae405370af6a3e
Joe Morris <joem@joemdesign.com>
2014-05-20 20:35:04 -0400

    SwitchCase
    Example

--- switchCase/main.m
+++ switchCase/main.m
@@ -1,8 +1,8 @@
 //
 //  main.m
-//  switchCase
+//  syntaxStudy
 //
-//  Created by Joe Morris on 5/12/14.
+//  Created by Joe Morris on 5/2/14.
 //  Copyright (c) 2014 joemdesign, llc. All rights reserved.
 //
 
@@ -10,11 +10,30 @@
 
 int main(int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        
+        int stormCategory = 4;
+        switch (stormCategory) {
+            case 1:
+                NSLog(@"Grab an umbrella");
+                break;
+            case 2:
+                NSLog(@"Now it's picking up");
+                break;
+            case 3:
+                NSLog(@"Batten down the latches");
+                break;
+            case 4:
+                NSLog(@"Batten down the latches");
+                break;
+            default:
+                break;
+        }
+        
+        
         
     }
     return 0;
